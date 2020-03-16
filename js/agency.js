dabdd6a5cabe4ddc29025a1e8a9e5104df9ad0fb
Artyom <slavasatane4434343@gmail.com>
2020-03-16 18:25:24 +0300

    end page

--- js/agency.js
+++ js/agency.js
@@ -1,17 +1,25 @@
 $(function () {
 	// Видео по наведению на карточку
-	if ($(window).width() > 1024 && $('.js-video-card').length) {
-
-		$('.js-video-card').on({
-			'mouseenter': function() {
-				var video = $(this).find('.video-card__object');
-				video[0].play();
-			},
-			'mouseleave': function() {
-				var video = $(this).find('.video-card__object');
-				video[0].pause();
-			}
-		});
-
+	var videoCard = $('.js-video-card');
+	var object = $('.video-card__object');
+	if (videoCard.length) {
+		if ($(window).width() > 1279) {
+	
+			videoCard.on({
+				'mouseenter': function() {
+					var video = $(this).children(object);
+					video[0].play();
+				},
+				'mouseleave': function() {
+					var video = $(this).children(object);
+					video[0].pause();
+				}
+			});
+		} else {
+			videoCard.each(function() {
+				var poster = $(this).children(object).data('poster');
+				$(this).children(object).attr("poster", poster);
+			});
+		}
 	}
 });
