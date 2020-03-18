ed6e47d42e22cf3677feba7bf7ceeb0a7f2ae7a3
Artyom <slavasatane4434343@gmail.com>
2020-03-18 11:17:12 +0300

    need end next btn

--- js/home.js
+++ js/home.js
@@ -1,14 +1,52 @@
 $(function () {
 
-	// Скролл по хешу
+	var root = $('.root');
+	var sects = ['.js-second', '#contacts'];
+	var length = sects.length;
+	var i = 0;
+	var next = $('.js-next-sect');
+
+	// Скролл по хешу - контакты
 	$(document).ready(function() {
 		if (window.hashId) {
 			setTimeout(function() {
-				$('.root').animate({
-					scrollTop: $(window.hashId).offset().top
-				}, 500);
+				animateSect($(window.hashId));
 			}, 1);
 		}
+	});
+
+	// Скролл по стрелке + по якорю
+	function animateSect(sect) {
+		root.stop().animate({
+			scrollTop: sect.offset().top + root.scrollTop()
+		}, 500);
+		return false;
+	}
+
+	next.on('click', function() {
+		animateSect($(sects[i]));
+	});
+
+	// root.on('scroll', function() {
+	// 	i === length ? next.hide() : next.show();
+	// 	if ($(this).scrollTop() >= $(sects[i]).offset().top && $(this).scrollTop() <= $(sects[i]).outerHeight(true)) {
+	// 		i++;
+	// 	}
+	// 	// if (i < length) {
+	// 	// 	next.show();
+	// 	// 	if ($(this).scrollTop() >= $(sects[i]).offset().top) {
+	// 	// 		i++;
+	// 	// 	}
+	// 	// } else {
+	// 	// 	next.hide();
+	// 	// }
+	// 	console.log(i)
+	// });
+
+	// Якорь - контакты
+	$('.js-anchor a').on('click', function(e) {
+		e.preventDefault();
+		animateSect($(sects[1]));
 	});
 
 	// Видео на главной
@@ -28,40 +66,6 @@ $(function () {
 	popupVideo.on('beforeClose', function() {
 		$(this).find('iframe')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
 		blockVideo.removeClass('transform');
-	});
-	
-	// Скролл по стрелке
-	var root = $('.root');
-	var sects = ['.js-second', '#contacts'];
-	var length = sects.length;
-	var i = 0;
-	var next = $('.js-next-sect');
-
-	function animateSect(sect) {
-		root.animate({
-			scrollTop: sect.offset().top + root.scrollTop()
-		}, 400);
-	}
-
-	next.on('click', function() {
-		animateSect($(sects[i]));
-	});
-
-	root.on('scroll', function() {
-		if (i < length) {
-			next.show();
-			if ($(this).scrollTop() >= $(sects[i]).offset().top) {
-				i++;
-			}
-		} else {
-			next.hide();
-		}
-		// console.log(i)
-	});
-
-	$('.js-anchor a').on('click', function(e) {
-		e.preventDefault();
-		animateSect($(sects[1]));
 	});
 
 });
