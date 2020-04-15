b0ffff00faa2dd2973be5ce8bb803c888a545823
Artyom2510 <SlavaSatane4434343@gmail.com>
2020-04-15 15:21:20 +0300

    page vacanie

--- js/agency.js
+++ js/agency.js
@@ -1,33 +1,4 @@
 $(function () {
-
-	var root = $('.root');
-
-	// Скролл по якорю
-	function animateSect(sect) {
-		root.stop().animate({
-			scrollTop: sect.offset().top + root.scrollTop()
-		}, 700);
-		return false;
-	};
-
-	// Скролл по хешу - контакты
-	$(document).ready(function() {
-		if (window.hashId) {
-			setTimeout(function() {
-				animateSect($(window.hashId));
-			}, 1500);
-		}
-	});
-
-	// Клик по якорю
-	// С использовании menu-nav__item ul li a выдавало ошибку
-	// При переходе с кейсов
-	$('.js-anchor-agency').on('click', function(e) {
-		e.preventDefault();
-		var hash = $(this).attr("href");
-		animateSect($(hash));
-	});
-
 	// Видео по наведению на карточку
 	var videoCard = $('.js-video-card');
 	var object = $('.video-card__object');
