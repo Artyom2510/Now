75ea2ce89b43cb4d74766df5942e92063cb55c12
Artyom <slavasatane4434343@gmail.com>
2020-07-13 10:08:27 +0300

    change settings slick

--- js/agency.js
+++ js/agency.js
@@ -25,11 +25,12 @@ $(function () {
 
 	if (slider.length && wWidth < 768) {
 		slider.slick({
+			rows: 3,
 			infinite: true,
 			dots: false,
 			arrows: false,
-			slidesToShow: 1,
-			slidesToScroll: 1,
+			slidesToShow: 3,
+			slidesToScroll: 3,
 			speed: 300,
 			cssEase: 'ease',
 			adaptiveHeight: true,
